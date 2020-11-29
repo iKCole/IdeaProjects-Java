7ddc094596fccce51285986cfe38eb8546ee2190
ikCole <ic8613@rit.edu>
2020-11-29 01:14:51 -0500

    create models for data

---
+++ ColUniversity/src/Models/Enrollment.java
@@ -0,0 +1,45 @@
+package Models;
+
+enum Grade {
+    A, B, C, D, F
+}
+public class Enrollment {
+    private int enrollmentID;
+    private int courseID;
+    private int studentID;
+    private Grade grade;
+
+    public int getEnrollmentID() {
+        return enrollmentID;
+    }
+
+    public void setEnrollmentID(int enrollmentID) {
+        this.enrollmentID = enrollmentID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+
+}
---
+++ ColUniversity/src/Models/Student.java
@@ -0,0 +1,53 @@
+package Models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Student extends Enrollment {
+    private int ID;
+    private String lastName;
+    private String firstMidName;
+    private Date enrollmentDate;
+
+    private List<Enrollment> enrollments;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstMidName() {
+        return firstMidName;
+    }
+
+    public void setFirstMidName(String firstMidName) {
+        this.firstMidName = firstMidName;
+    }
+
+    public Date getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+
+    public void setEnrollments(List<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
+}
