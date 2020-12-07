8d502413a9b20579cb13532deafff973d8637d68
ikCole <ic8613@rit.edu>
2020-12-06 20:28:57 -0500

    function that displays and accepts input

--- AmazonShopping/src/com/ikcole/Main.java
+++ AmazonShopping/src/com/ikcole/Main.java
@@ -3,33 +3,44 @@ package com.ikcole;
 import Models.Product;
 import Models.Products;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static Scanner READ = new Scanner(System.in);
     static Product product = new Product();
     static Products productStore = new Products();
+    static List<Product> productList = new ArrayList<>();
 
     public static void main(String[] args) {
         addProductStore();
+        viewProductDetails();
 
     }
 
     public static void addProductStore() {
-        int press;
-        do {
+        Random random = new Random();
             System.out.println("\n\t***** Add item *****");
+            product.setProductID(random.nextInt(100));
             System.out.print("Enter the name of the item you want to add: ");
             product.setProductName(READ.nextLine());
             System.out.print("Enter the price: ");
-            product.setProductPrice(READ.nextBigDecimal());
+            product.setProductPrice(BigDecimal.valueOf(READ.nextDouble()));
             System.out.print("How many " + product.getProductName() + " are in stock: ");
             product.setProductStock(Integer.parseInt(READ.next()));
-            System.out.println("\nAdd more?\nPress 1 to continue\nPress 9 to exit");
             productStore.addProduct(product);
-            press = Integer.parseInt(READ.next());
-        } while (press != 9);
+    }
+
+    public static void viewProductDetails() {
+        System.out.println(" **** Product List ****");
+        productList = productStore.viewAllProducts();
+        for (Product list: productList
+             ) {
+            System.out.println("==| " + list.getProductID() + "\t" + list.getProductName() + "\t" + list.getProductPrice() +
+                    "\t" + list.getProductStock());
+        }
     }
 }
