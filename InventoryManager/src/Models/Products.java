44e4c6f697e0b8d9ad63f9d82b0245cdab66e4ec
ikCole <ic8613@rit.edu>
2020-12-22 19:25:44 -0500

    initializing new project

---
+++ InventoryManager/src/Models/Products.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class Products {
+    private static String productName;
+    private static String productDescription;
+    private static Enum<Category> productCategory;
+    private static int productQuantity;
+}
