3da5dc02c129fd8a1759c64833e645d4da8904fc
ikCole <ic8613@rit.edu>
2021-01-03 18:13:20 -0500

    Created Print functions

--- college-student-management/src/com/ikcole/Models/ProfessorAccess.java
+++ college-student-management/src/com/ikcole/Models/ProfessorAccess.java
@@ -22,28 +22,48 @@ public class ProfessorAccess {
         student.setId(Integer.parseInt(sc.next()));
         student.setFirstName(readData("Enter Student's First Name: "));
         student.setLastName(readData("Enter Student's Last Name: "));
-        System.out.print(">>> New Email and Password will be set for this.\n\t >| Enter No. of characters for " + "password: ");
+        System.out.print(">>> New Email and Password will be set for this student.\n >| Enter No. of characters for" +
+                " " + "password: ");
         student.setPassword(student.setRandomPassword(sc.nextInt()));
         student.setEmailAddress(student.generateEmail());
-
+        System.out.println(student.toString());
         studentList.add(student);
     }
 
     public void viewAllStudent() {
-        System.out.println("---------------------------------------------------------------");
-        System.out.printf("%-20s %20s %20s", "FULL NAME", "EMAIL", "PASSWORD\n");
-        System.out.println("---------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.printf("%-12s %-20s %-20s %-20s", "STUDENT ID", "NAME", "EMAIL ID", "PASSWORD");
+        System.out.println("");
+        System.out.println("----------------------------------------------------------------------------");
         for(Student student : studentList) {
-            System.out.format("%-20s %20s %20s", (student.getFirstName() + " " + student.getLastName()),
-                    student.getEmailAddress(), student.getPassword());
+            System.out.format("%-12s %-20s %-20s %-20s\n",
+                    student.getId(), (student.getFirstName() + " " + student.getLastName()), student.getEmailAddress(), student.getPassword());
         }
+    }
+
+    public void viewById() {
+        int findId = Integer.parseInt(readData("Enter Student ID to View Information: "));
+        for(Student studentByID : studentList) {
+            if(findId == studentByID.getId()) {
+                System.out.println("----------------- Student Info for ID " + findId + "--------------------");
+                System.out.println("----------------------------------------------------------------------------");
+                System.out.printf("%8s %-20s %-20s %-20s", "STUDENT ID", "NAME", "EMAIL ID", "PASSWORD");
+                System.out.println("");
+                System.out.println("----------------------------------------------------------------------------");
+                System.out.format("%8s %-20s %-20s %-20s", student.getId(),
+                        (student.getFirstName() + " " + student.getLastName()), student.getEmailAddress(), student.getPassword());
+            }
+        }
+    }
+
+    public void removeStudent(int id) {
+        studentList.removeIf(n -> n.getId() == id);
+        viewAllStudent();
     }
 
     public String readData(String read) {
         System.out.print(read);
         return sc.next();
     }
-
-
 
 }
