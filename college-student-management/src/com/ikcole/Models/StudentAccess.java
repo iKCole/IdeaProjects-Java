08f85d1a1d13edef3a60911fbeaa7cde1ba077b9
ikCole <ic8613@rit.edu>
2021-01-05 20:46:42 -0500

    fix matching errors

--- college-student-management/src/com/ikcole/Models/StudentAccess.java
+++ college-student-management/src/com/ikcole/Models/StudentAccess.java
@@ -1,26 +1,27 @@
 package com.ikcole.Models;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentAccess {
     private final Student student = new Student();
-    private final ProfessorAccess authStudent = new ProfessorAccess();
+    ProfessorAccess authStudent = new ProfessorAccess();
     Scanner read = new Scanner(System.in);
 
-    public void studentAccess() {
+    public void studentLogin(List<Student> accessStudent) {
         System.out.println("\nEnter your Email: ");
         String email = read.nextLine();
         System.out.println("Enter your Password: ");
         String password = read.nextLine();
-        for(Student matchBy : authStudent.getStudentList()) {
-            if(!email.equals(matchBy.getEmailAddress())) {
+        for(Student student : accessStudent) {
+            if(!email.equals(student.getEmailAddress()) && !password.equals(student.getPassword())) {
                 System.out.println("Invalid Email or Password");
             }
             else {
                 System.out.println("\n Login Successful\n Here is your Student Profile");
-//                authStudent.displayHeader();
-//                System.out.format("%-12s %-15s %-20s %-20s", student.getId(),
-//                        (student.getFirstName() + " " + student.getLastName()), student.getEmailAddress(), student.getPassword());
+                authStudent.displayHeader();
+                System.out.format("%-12s %-15s %-20s %-20s", student.getId(),
+                        (student.getFirstName() + " " + student.getLastName()), student.getEmailAddress(), student.getPassword());
             }
         }
     }
